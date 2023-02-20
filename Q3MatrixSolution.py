c7a6fce0462055881fe6b25dd5c55732a9356c8f
hunterdih <54923060+hunterdih@users.noreply.github.com>
2023-02-20 17:23:06 -0500

    Update Q3MatrixSolution.py

--- Q3MatrixSolution.py
+++ Q3MatrixSolution.py
@@ -8,17 +8,16 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import pandas as pd
 
-REG_ALPHA = 0.5
+REG_ALPHA = 0.025
 
-
-def load_and_process_data(data_path='0', delim=','):
+def load_and_process_data(data_path='0', delim=',', normalize=True):
     if data_path == '0':
         print(f'ERROR: No Dataset Passed')
     data = read_csv(data_path, delimiter=delim)  # Load the data into a pandas dataframe
     class_column_name = data.columns.values.tolist()[
         -1]  # Get the name of the labels column (should be the last column)
     min_class_number = data[class_column_name].min()  # Get min class index for counting
-    max_class_number = data[class_column_name].max()+1  # Get max class index for counting
+    max_class_number = data[class_column_name].max() + 1  # Get max class index for counting
     print(f'{min_class_number=}, {max_class_number=}, entries={data.shape[0]}')
 
     print(f'Finding Class Priors...')
@@ -30,13 +29,14 @@ def load_and_process_data(data_path='0', delim=','):
     print(f'Class Priors Determined...')
     print(f'\n{priors=}')
 
-    print(f'Normalizing Data to Zero Mean, 1 Cov')
-    class_column = data[class_column_name]
-    data = data.drop(columns=[class_column_name])
-    data = pd.DataFrame(StandardScaler().fit_transform(data.loc[:].values),
-                        columns=data.columns)
-    data[class_column_name] = class_column
-    print(f'Data Normalized...')
+    if normalize:
+        print(f'Normalizing Data to Zero Mean, 1 Cov')
+        class_column = data[class_column_name]
+        data = data.drop(columns=[class_column_name])
+        data = pd.DataFrame(StandardScaler().fit_transform(data.loc[:].values),
+                            columns=data.columns)
+        data[class_column_name] = class_column
+        print(f'Data Normalized...')
 
     # Calculate the number of features for identity matrix
     features = data.shape[1] - 1  # Subtract 1 to exclude the class labels
@@ -71,7 +71,7 @@ def map_classify(data, lmda_mat, class_means, class_covs, priors):
     return_dataframe = pd.DataFrame(data, columns=data.columns)
     return_dataframe = return_dataframe
 
-    data_select = return_dataframe.values[:,:-1]
+    data_select = return_dataframe.values[:, :-1]
     pre_loss_values = []
     for prior in range(len(priors)):
         lmda_select = lmda_mat[prior]
@@ -79,7 +79,6 @@ def map_classify(data, lmda_mat, class_means, class_covs, priors):
         gau_values = np.asarray(multivariate_normal.pdf(data_select, class_means[prior], class_covs[prior]).tolist())
         hold_mat = np.zeros_like(gau_values)
         for risk_prior in range(len(priors)):
-
             mean_select = class_means[prior]
             covs_select = class_covs[prior]
             prior_select = priors[prior]
@@ -134,7 +133,8 @@ def generate_graphs(data, confusion_mat):
     guess_column = data['guess']
     data = data.drop(columns=[class_column_name, 'guess', 'choice'])
     PCA3 = PCA(n_components=3).fit_transform(data)
-    PCA3_dataframe = pd.DataFrame(data=PCA3, columns=['Principle Component 1', 'Principle Component 2', 'Principle Component 3'])
+    PCA3_dataframe = pd.DataFrame(data=PCA3,
+                                  columns=['Principle Component 1', 'Principle Component 2', 'Principle Component 3'])
     PCA3_dataframe[class_column_name] = class_column
     PCA3_dataframe['guess'] = guess_column
 
@@ -149,8 +149,10 @@ def generate_graphs(data, confusion_mat):
 
     for label in range(min_class_number, max_class_number + 1):
         interest = (PCA3_dataframe[PCA3_dataframe[class_column_name] == label]).drop(columns=[class_column_name])
-        correct_PCA.append(PCA3_dataframe[(PCA3_dataframe[class_column_name] == label) & (PCA3_dataframe['guess'] == 1)])
-        incorrect_PCA.append(PCA3_dataframe[(PCA3_dataframe[class_column_name] == label) & (PCA3_dataframe['guess'] == 0)])
+        correct_PCA.append(
+            PCA3_dataframe[(PCA3_dataframe[class_column_name] == label) & (PCA3_dataframe['guess'] == 1)])
+        incorrect_PCA.append(
+            PCA3_dataframe[(PCA3_dataframe[class_column_name] == label) & (PCA3_dataframe['guess'] == 0)])
         split_PCA.append(interest)
     print(f'Splitting PCA by Labels Done...')
 
@@ -251,11 +253,28 @@ def generate_graphs(data, confusion_mat):
 
 if __name__ == '__main__':
     # CONFIGURATIONS FOR DATASET SPECIFIC FUNCTIONS
-
+    optimal_reg = False
+    normalize_inputs = True
     # Import the dataset
     dataset_path = Path(
-        r'C:\Users\David Hunter\OneDrive\Northeastern Classes\Graduate\EECE5644MachineLearning\Homework1\winequality-white.csv')  # Path to the dataset
-    dataset, means, covariances, reg_covariances, class_priors = load_and_process_data(dataset_path, ';')
+        r'C:\Users\David Hunter\OneDrive\Northeastern Classes\Graduate\EECE5644MachineLearning\Homework1\HumanActivityData.csv')
+    reg_alpha_list = []
+    if optimal_reg:
+        for norm in range(1, 50):  # Path to the dataset
+            REG_ALPHA = norm * 0.0001
+            dataset, means, covariances, reg_covariances, class_priors = load_and_process_data(dataset_path, ',', normalize=normalize_inputs)
+
+            # Generate the lambda matrix
+            lambda_matrix = np.ones((len(class_priors), len(class_priors)))
+            lambda_matrix[np.diag_indices_from(lambda_matrix)] = 0
+
+            dataset, loss_matrix = map_classify(dataset, lambda_matrix, means, reg_covariances, class_priors)
+            display_confusion_matrix, loss = get_confusion_matrix_loss(loss_matrix, class_priors)
+            reg_alpha_list.append(loss)
+
+        REG_ALPHA = reg_alpha_list.index(min(reg_alpha_list))*0.001
+        print(f'{REG_ALPHA=}, {reg_alpha_list=}')
+    dataset, means, covariances, reg_covariances, class_priors = load_and_process_data(dataset_path, ',', normalize=normalize_inputs)
 
     # Generate the lambda matrix
     lambda_matrix = np.ones((len(class_priors), len(class_priors)))
@@ -263,7 +282,6 @@ if __name__ == '__main__':
 
     dataset, loss_matrix = map_classify(dataset, lambda_matrix, means, reg_covariances, class_priors)
     display_confusion_matrix, loss = get_confusion_matrix_loss(loss_matrix, class_priors)
-
     rounded_loss = round(loss, 4)
     print(f'Loss: {rounded_loss * 100}%')
     print(f'Confusion Matrix:\n{display_confusion_matrix}')
